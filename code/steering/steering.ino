a5f6c298f90efad0cd691bc010ec5273257d0aca
Jakob Nilsson <jakob.a.nilsson@gmail.com>
2018-09-09 22:41:35 +0200

    case and steering code

--- code/steering/steering.ino
+++ code/steering/steering.ino
@@ -1,40 +1,41 @@
 // this constant won't change:
 const int  pinLeft = 4;    // the pin that the pushbutton is attached to
 const int  pinRight = 2;    // the pin that the pushbutton is attached to
-boolean flagLeft = 0;
-boolean flagRight = 0; 
-
 const int wheelCirc = 221; //Wheel cicumferance i mm
 const int pulses = 20; // pulses per rotation
 const int travel = wheelCirc/pulses; // traveled distance per pulse
-const int forward = 50;
-const int backward = 137;
-const int stopped = 90;
-int currentSpeed = 0;
-int left = 0;
-int right =0;
-unsigned long timerL = 0;
-unsigned long timerR = 0;
+const int forward = 50; // PWM setting for the motor driver
+const int backward = 137;// PWM setting for the motor driver
+const int stopped = 90; // PWM setting for the motor driver
 
 
-#include <Servo.h>
-
-Servo myservoLeft;  
-Servo myservoRight;  
+const int pingPin = 7; //the arduino pin that communicates with the ultrasound sensor:
+const long timeout = 400 * 29 * 2; //Timeout in microseconds for a max range of 400 cm
+long duration, mm;
 
 // Variables will change:
-int buttonPushCounter = 0;   // counter for the number of button presses
-int buttonState = 0;         // current state of the button
-int lastButtonState = 0;     // previous state of the button
-unsigned long timer = 0;
-int pos =90;
-int speedCount = 0;
-int countLast = 0;
-int count = 0;
-unsigned long time1 = 0;
+int left = 0; //how far to drive 
+int right =0; //how far to drive 
+unsigned long timerL = 0; // debounce timer
+unsigned long timerR = 0; // debounce timer
+boolean flagLeft = 0; // for the pulse counter
+boolean flagRight = 0; // for the pulse counter
+int servoPos = 90; //goes from 0 to 180 deg, 90 is in the middle (actually 100)
+int scanStep = 15; //the ultrasound can see 15 deg
+boolean scanDir = 0; // the direction the scanner is moving
+int scanTime = 150; // for timing of the scanner
+unsigned long scanTimer = 0; // for timing of the scanner
+int dir = 0; //direction
+int dirLongest = 0;
+int longest = 0;
+int distance =0; 
+int reporter = 500;
 
+#include <Servo.h>
+Servo motorLeft;  
+Servo motoRight;  
+Servo myservo;  //holds the ultrasound sensor
 
- 
 void setup() {
   // put your setup code here, to run once:
         // initialize serial communication:
@@ -42,10 +43,11 @@ void setup() {
         // initialize the button pin as a input:
     pinMode(pinLeft, INPUT);
         pinMode(pinRight, INPUT);
-  myservoLeft.attach(10); 
-  myservoRight.attach(9); 
-  left  = 20; // drive X cm forward on the left side
-right = 10; // drive X cm forward on the right side
+  motorLeft.attach(10); // motor driver 
+  motoRight.attach(9); // motor driver 
+    myservo.attach(3); // Servo on pin 3
+left  = 0; // drive X cm forward on the left side
+right = 0; // drive X cm forward on the right side
 
 }
 
@@ -54,13 +56,17 @@ void loop() {
 
 drive();
 counter();
-if (millis() % 600 > 300){
-Serial.print(left);
-Serial.print(" " );
-Serial.print(right);
-Serial.println();
+int directionDrive = scan();
+if(directionDrive != 0){  //Change so that it can use the measured longest distance and middle direction
+  if (directionDrive < 0){
+    right= -directionDrive;
+      // right
+  }else {
+left= directionDrive;
+   // left
+  }
 }
- 
+
 }
 
 
@@ -68,23 +74,25 @@ Serial.println();
 void drive(){
 
 if (left > right && left > 0 ) {
-    myservoLeft.write(backward); //soldered it backward
+    motorLeft.write(backward); //soldered it backward
 } 
 if (right > left && right > 0 ) {
-    myservoRight.write(forward); 
+    motoRight.write(forward); 
 } 
 
 if (right == left && left > 0) {
-    myservoRight.write(forward); 
-    myservoLeft.write(backward); //soldered it backward
+    motoRight.write(forward); 
+    motorLeft.write(backward); //soldered it backward
 } 
 
 if (left  < 0 ) {
-    myservoLeft.write(stopped); 
+    motorLeft.write(stopped); 
+    left = 0;
 } 
 if (right  < 0 ) {
-    myservoRight.write(stopped); 
-    Serial.println("here");
+    motoRight.write(stopped); 
+    right= 0;
+ 
 } 
 
   
@@ -109,4 +117,63 @@ int counter(){
  flagRight = rightState;
 }
 
+int scan(){
+if(millis() - scanTimer > scanTime){// only act if it's time to scan
+  scanTimer = millis(); // reset the timer
+  myservo.write(servoPos);              // tell servo to go to position in variable 'servoPos'
+ if (scanDir == 0) {
+     servoPos = servoPos - scanStep;
+     dir = dir -1 ;
+    }else {
+       servoPos = servoPos + scanStep;
+       dir = dir+1;
+    }
+
+
+distance = ping();
+if (distance > longest ){
+longest = distance;
+dirLongest = dir; // current dir
+}
+
+    
+ if (servoPos >= 135 || servoPos <= 45){   //Invert scanDir if we have reached either end
+    scanDir = !scanDir; 
+    return dirLongest;
+     longest = 0; // reset
+    } else {
+      return 0;}
+
+
+
+}
+}
+
+int ping(){
+
+  // The sensor is triggered by a HIGH signal for 10 microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse and 
+  // to reset the sensor if it didn't register an echo from the last ping:
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(10);  //replace with timer
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10); //replace with timer
+  digitalWrite(pingPin, LOW);
+  // The same pin is used to read the signal from the Sensor: A HIGH pulse
+  // whose duration is the time (in microseconds) from the sending of the ping
+  // to the reception of its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH,timeout);
+  // convert the time into a distance
+  mm = microsecondsToMillimeters(duration);
+return mm;
+}
+
+long microsecondsToMillimeters(long microseconds) {
+  // The speed of sound is 343 m/s or 2.9215 microseconds per millimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds * 10000 /29215 / 2;
+}
 
